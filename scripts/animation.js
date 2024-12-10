c37928018102a0e1f60007393fe6265e92dfe9f4
Vitor Sbeghi <126426164+SbgVit0r@users.noreply.github.com>
2024-12-10 15:07:49 -0300

    Site Completo

---
+++ scripts/animation.js
@@ -0,0 +1,12 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const elementos = document.querySelectorAll('.animado');
+elementos.forEach((el) => observer.observe(el));
